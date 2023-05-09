7e7a29f5643edb0099bb271a959de3fc7a4d72a8
Guillermo Hasbun <hasbun.guillermo@gmail.com>
2023-05-08 23:22:03 -0400

    Refine initial query

--- server/routes/orders.js
+++ server/routes/orders.js
@@ -6,7 +6,12 @@ const Router = require('express-promise-router');
 const router = new Router();
 
 /*
-`select *, (select json_agg(b) as borrowed from (select * borrowed_books where borrower_id = ${testId}) as b) from borrowed_books where owner_id =${testId};`)
+select
+  (select json_agg(o) as borrowed
+   from (select * from borrowed_books where owner_id = 1) as o),
+  (select json_agg(b) as borrowed
+   from (select * from borrowed_books where borrower_id = 1) as b)
+   from borrowed_books where owner_id = 1;
 */
 
 
@@ -16,10 +21,14 @@ router.get('/orders', async (req, res) => {
   console.log(req.cookies)
   var testId = 1;
   var sendList = {};
-  client.query(`select *,
+  client.query(`
+  select
+  (select json_agg(o) as owned
+   from (select * from borrowed_books where owner_id = ${testId}) as o),
   (select json_agg(b) as borrowed
-   from (select * from borrowed_books where borrower_id = 1) as b)
-   from borrowed_books where owner_id = 1;`)
+   from (select * from borrowed_books where borrower_id = ${testId}) as b)
+   from borrowed_books where owner_id = ${testId};
+  `)
     .then((orders) => {
       console.log(orders.rows)
       sendList.borrowed = orders.rows;
@@ -31,27 +40,3 @@ router.get('/orders', async (req, res) => {
 
 // export router to import on server file
 module.exports = router;
-
-/*
-
-this is my SDC query. I'm trying to see how I set up my aggregate calls and apply them here
-
-const singleProduct = (req, res) => {
-  cache.get(`${req.params.product_id}SINGLE`)
-    .then((store) => {
-      if (store === null) {
-        client.query(`select *, (select json_agg(f) as features from (select feature, value from features where features.product_id = productlist.product_id) as f) from productlist where product_id =${req.params.product_id};`)
-          .then((productData) => {
-            // send first, then save the data into the cache
-            res.status(200).json(productData.rows[0]);
-            cache.set(`${req.params.product_id}SINGLE`, JSON.stringify(productData.rows[0]));
-          })
-          .catch((err) => { res.sendStatus(500); throw err; });
-      } else {
-        res.status(200).json(JSON.parse(store));
-      }
-    })
-    .catch(err => res.sendStatus(500))
-};
-
-*/
