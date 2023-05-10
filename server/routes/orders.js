37ca09ad26737d1a40d9049f152ea29b72a8ac76
Guillermo Hasbun <hasbun.guillermo@gmail.com>
2023-05-09 22:35:16 -0400

    Make order's main query

--- server/routes/orders.js
+++ server/routes/orders.js
@@ -9,17 +9,54 @@ orders.get('/orders', async (req, res) => {
   const id  = req.query;
   console.log(id);
   console.log(req.cookies)
-  var testId = 1;
+  var testId = 7;
   var sendList = {};
   client.query(`
   select
-  (select json_agg(o) as owned
-   from (select * from borrowed_books where owner_id = ${testId}) as o),
 
-  (select json_agg(b) as books
-   from (select * from books where books.book_id = borrowed_books.book_id) as b)
+  (select json_agg(o) as owned
+   from (select borrow_date, return_date, shipped_to_borrower, shipped_to_owner, (
+  (select json_agg(d) as details
+   from (select author, title from authors INNER JOIN books ON authors.isbn = books.isbn
+    where books.book_id = borrowed_books.book_id) as d))
+    from borrowed_books where owner_id = ${testId}) as o),
+
+  (select json_agg(b) as borrowed
+   from (select borrow_date, return_date, shipped_to_borrower, shipped_to_owner, (
+  (select json_agg(d) as details
+   from (select author, title from authors INNER JOIN books ON authors.isbn = books.isbn
+    where books.book_id = borrowed_books.book_id) as d))
+    from borrowed_books where borrower_id = ${testId}) as b)
+
    from borrowed_books where owner_id = ${testId};
   `)
+
+  /*
+
+ select
+
+  (select json_agg(o) as owned
+   from (select *, (
+  (select json_agg(d) as details
+   from (select author, title from authors INNER JOIN books ON authors.isbn = books.isbn
+    where books.book_id = borrowed_books.book_id) as d))
+    from borrowed_books where owner_id = ${testId}) as o),
+
+
+  (select json_agg(b) as borrowed
+   from (select *, (
+  (select json_agg(i) as info
+   from (select author, title from authors INNER JOIN books ON authors.isbn = books.isbn
+    where books.book_id = borrowed_books.book_id) as i))
+    from borrowed_books where borrower_id = ${testId}) as b)
+
+   from borrowed_books where owner_id = ${testId};
+
+
+
+borrow_date, return_date, shipped_to_borrower, shipped_to_owner
+
+  */
     .then((orders) => {
       console.log(orders.rows)
       sendList.borrowed = orders.rows;
