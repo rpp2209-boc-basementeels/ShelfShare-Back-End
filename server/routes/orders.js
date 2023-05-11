41821bd475d1f703fc8f4b79983609890db0a9ed
Guillermo Hasbun <hasbun.guillermo@gmail.com>
2023-05-11 16:14:22 -0400

    Filter books by loaned, borrowed, and pending

--- server/routes/orders.js
+++ server/routes/orders.js
@@ -5,12 +5,43 @@ const Router = require('express-promise-router');
 // create new router instance
 const orders = new Router();
 
+//pending helper function
+// this takes in an object, splits it into valid and pending orders
+let sendSort = (obj) => {
+  //obj.loaned and obj.borrowed are arrays of obj
+  var clean = {
+    loaned: [],
+    borrowed: [],
+    pending:[]
+  };
+
+  // loaned books
+  for (var i = 0; i < obj.loaned.length; i++) {
+    let loanedBook = obj.loaned[i];
+    if (loanedBook.shipped_to_borrower === false && loanedBook.shipped_to_borrower === false) {
+      clean.pending.push(loanedBook);
+    } else {
+      clean.loaned.push(loanedBook)
+    }
+  }
+  // borrowed books
+  for (var j = 0; j < obj.borrowed.length; j++) {
+    let borrowedBook = obj.borrowed[j];
+    if (borrowedBook.shipped_to_borrower === false && borrowedBook.shipped_to_borrower === false) {
+      clean.pending.push(borrowedBook);
+    } else {
+      clean.borrowed.push(borrowedBook)
+    }
+  }
+  return clean;
+}
+
 orders.get('/orders/:user_id', async (req, res) => {
   let user = req.params.user_id;
   client.query(`
   select
 
-  (select json_agg(o) as owned
+  (select json_agg(o) as loaned
    from (select borrow_date, return_date, shipped_to_borrower, shipped_to_owner, (
   (select json_agg(d) as details
    from (select author, title from authors INNER JOIN books ON authors.isbn = books.isbn
@@ -27,7 +58,10 @@ orders.get('/orders/:user_id', async (req, res) => {
    from borrowed_books where owner_id = ${user};
   `)
     .then((orders) => {
-      res.send(orders.rows).status(200);
+      // console.log(orders.rows[0])
+
+      // console.log(Object.keys(sendList.loaned[0]))
+      res.send(sendSort(orders.rows[0])).status(200);
     })
     .catch((err) => { res.sendStatus(500); throw err; });
   })
