ab0ba449cda4cd3d008decb4bfb23dde6bc1289f
Maddie Sime <madisonfaithsime@gmail.com>
2023-05-15 12:26:57 -0400

    Added functionality for GET /bookReviews/:book_id

--- server/routes/profileRouter.js
+++ server/routes/profileRouter.js
@@ -15,14 +15,22 @@ profileRouter.get('/reviews/:username', async (req, res) => {
 profileRouter.get('/personalInformation/:username', async (req, res) => {
     var username = req.params.username;
     try {
-        const userInfo = await db.query(`SELECT username, photo_url, email, first_name, last_name, gender, age, is_library, address FROM users WHERE username = '${username}'`);
+        const userInfo = await db.query(`SELECT username, photo_url, email, first_name, last_name, gender, age, is_library, line_1, line_2, city, state, postal, country FROM users WHERE username = '${username}'`);
         res.status(200).send(userInfo.rows);
     } catch (error) {
         res.status(500).send(error);
     }
 });
 
-// every time a review is added, need to find isbn in book table and take that book id and insert into review table with review data
-// so that book image can be looked up for get requests
+profileRouter.get('/bookReviews/:book_id', async (req, res) => {
+    var book_id = req.params.username;
+    try {
+        const bookReviews = await db.query(`SELECT * FROM reviews WHERE book_id = ${book_id}`);
+        res.status(200).send(bookReviews.rows);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+});
+
 module.exports = profileRouter;
 
