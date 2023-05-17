8fe878889838804ccf4a6cc501bf53d6d43029af
fig <arielfishkin@gmail.com>
2023-05-16 18:51:54 -0700

    backend handling of borrow request

--- server/routes/usage.js
+++ server/routes/usage.js
@@ -21,5 +21,22 @@ usageRouter.get('/usage/genre/', async (req, res) => {
   res.sendStatus(200);
 })
 
+usageRouter.post('/usage/records', async (req, res) => {
+  console.log('request body', req.body);
+  const isbn = req.body.isbn;
+  const date = req.body.date;
+  const genre = req.body.genre;
+
+  try {
+    const addRecord = await db.query(`INSERT INTO usage (isbn, user_age, user_gender, book_genre, transaction_date)
+    VALUES ($1, $2, $3, $4, $5)`, [isbn, 43, 'non-binary', genre, date]);
+    console.log(addRecord);
+    res.sendStatus(201);
+  } catch (error) {
+    res.sendStatus(500);
+  }
+
+})
+
 // export router to import on server file
 module.exports = usageRouter;
